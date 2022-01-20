895b826f887f8217830945c41cc85aa91ed7b9ea
Martin Lenart <martin_lenart@icloud.com>
2022-01-20 16:17:09 +0100

    Add project files.

---
+++ ADOPXF_PrimeNumbersService/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+using ADOPXF_E_PrimeNumbersService.Models;
+using ADOPXF_E_PrimeNumbersService.Services;
+
+namespace ADOPXF_E_PrimeNumbersService
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var service = new PrimeNumberService();
+            var progressReporter = new Progress<(float, string)>(value =>
+            {
+                Console.WriteLine(value.Item2);
+            });
+
+            var progressReporter1 = new Progress<float>(value =>
+            {
+                Console.WriteLine($"Reading batches progress: {value}");
+            });
+
+            Console.WriteLine("Get Primes using Progress reporter:");
+            await service.DisplayPrimeCountsAsync(10, progressReporter);
+
+            Console.WriteLine();
+            Console.WriteLine("Get Primes using Progress reporter and List<PrimeBatch>:");
+            var batches = await service.GetPrimeBatchCountsAsync(10, progressReporter1);
+
+            Console.WriteLine();
+            Console.WriteLine("List<PrimeBatch> Content:");
+            Console.WriteLine($"Number of batches {batches.Count}");
+            batches.ForEach(batch => Console.WriteLine(batch.ToString()));
+
+        }
+    }
+}
---
+++ ADOPXF_PrimeNumbersService/Services/PrimeNumberService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+using ADOPXF_E_PrimeNumbersService.Models;
+
+namespace ADOPXF_E_PrimeNumbersService.Services
+{
+    public class PrimeNumberService
+    {
+        public Task<List<PrimeBatch>> GetPrimeBatchCountsAsync(int NrOfBatches) => GetPrimeBatchCountsAsync(NrOfBatches, null);
+        public Task<List<PrimeBatch>> GetPrimeBatchCountsAsync(int NrOfBatches, IProgress<float> onProgressReporting)
+        {
+            return Task.Run(async () =>
+            {
+                var batchList = new List<PrimeBatch>();
+                for (int i = 0; i < NrOfBatches; i++)
+                {
+                    var batch = new PrimeBatch { BatchStart = i * PrimeBatch.BatchSize + 2};
+                    batch.NrPrimes = await GetPrimesCountAsync(batch.BatchStart, PrimeBatch.BatchSize);
+                    batchList.Add(batch);
+
+                    float fProgress = ((float) i + 1) / NrOfBatches;
+                    onProgressReporting?.Report(fProgress);
+                }
+                return batchList;
+            });
+        }
+        public Task DisplayPrimeCountsAsync(int NrOfBatches, IProgress<(float, string)> onProgressReporting)
+        {
+            //Notice I can use async in Lambda Expression
+            return Task.Run(async () =>
+            {
+                for (int i = 0; i < NrOfBatches; i++)
+                {
+                    float k = i+1;
+                    int nrprimes = await GetPrimesCountAsync(i * 1000000 + 2, 1000000);
+                    onProgressReporting.Report((k/NrOfBatches, $"{nrprimes:N0} primes between {(i * 1000000):N0} and {((i + 1) * 1000000 - 1):N0}"));
+                }
+            });
+        }
+
+        public Task<int> GetPrimesCountAsync(int start, int count)
+        {
+            return Task.Run(() =>
+               ParallelEnumerable.Range(start, count).Count(n =>
+                 Enumerable.Range(2, (int)Math.Sqrt(n) - 1).All(i => n % i > 0)));
+        }
+
+        public Task<List<int>> GetPrimesAsync(int start, int count)
+        {
+            return Task.Run(() =>
+               ParallelEnumerable.Range(start, count).Where(n =>
+                 Enumerable.Range(2, (int)Math.Sqrt(n) - 1).All(i => n % i > 0)).ToList<int>());
+        }
+    }
+}
